92d7774e929bbf7111af255c6e4bef1aacd65a16
Riya Rana <riya.rana@ihsmarkit.com>
2023-03-17 12:19:25 +0530

    OrderService tests

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            //Arrange
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+
+            //Act
+            var order = new Order();
+            orderService.PlaceOrder(order);
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
