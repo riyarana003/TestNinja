73ac6ace43821d974891679b479c063d82cb630f
Riya Rana <riya.rana@ihsmarkit.com>
2023-03-17 17:16:52 +0530

    Product tests

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            //Arrange
+            var product = new Product { ListPrice = 100};
+
+            //Act 
+            var result = product.GetPrice(new Customer { IsGold = true });
+            
+            //Assert
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
